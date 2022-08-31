292c64b5accb49f63c11ed6f145c19b5d661b78c
hikariuchino <irakih9140@icloud.com>
2022-08-31 18:28:14 +0900

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+  mount_uploader :image, ImageUploader
+  has_many :posts, dependent: :destroy
+  validates :name, presence: true 
+  validates :profile, length: { maximum: 200 } 
+
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
+  def already_liked?(post)
+    self.likes.exists?(post_id: post.id)
+  end
+  has_many :comments, dependent: :destroy
+end
+
